632ae1a2262b1cdd3eed2d902b28be22dbc165e8
Fullchee Zhang <fullchee@gmail.com>
2020-01-29 16:07:09 -0500

    Structure the data to always have keywords

--- scripts/link-parsing.js
+++ scripts/link-parsing.js
@@ -1,11 +1,33 @@
 const fs = require("fs");
-
+const path = require("path");
 // added an id to every object
-const links = JSON.parse(fs.readFileSync("./links.json"));
-let i = 0;
-links.forEach(link => {
-  link.id = i++;
-  return link;
-});
+const links = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "..", "src", "links.json"))
+);
+function addIds() {
+  let i = 0;
+  links.forEach(link => {
+    link.id = i++;
+    return link;
+    fs.writeFileSync("./links2.json", JSON.stringify(links));
+  });
+}
+function addKeywords() {
+  links.forEach(link => {
+    if (!link.keywords) {
+      link.keywords = [];
+    }
+  });
+  fs.writeFileSync("./links2.json", JSON.stringify(links));
+}
 
-fs.writeFileSync("./links2.json", JSON.stringify(links));
+function getAllKeywords() {
+  let keywords = links.reduce((acc, link) => {
+    console.log(link);
+    return [...acc, ...link.keywords];
+  }, []);
+  console.log(keywords);
+}
+
+// addKeywords();
+getAllKeywords();
